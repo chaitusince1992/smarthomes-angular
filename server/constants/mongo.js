48a32dd4039f0caa437871b6fc680f794316fbb6
Krishna Chaitanya Kotu <krishna.kotu@imaginea.com>
2019-08-26 22:27:26 +0530

    new mongo connection

--- server/constants/mongo.js
+++ server/constants/mongo.js
@@ -4,7 +4,8 @@ var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 
 mongo.getCollection = function (callbackSuccess, callbackError) {
-    MongoClient.connect("mongodb://localhost:27017/",
+    MongoClient.connect("mongodb+srv://chaitu257:cmk357@smartgrid-shqik.mongodb.net/test?retryWrites=true&w=majority",
+    // MongoClient.connect("mongodb://localhost:27017/",
         { useNewUrlParser: true, useUnifiedTopology: true },
         function (err, client) {
             if (err) {
@@ -18,3 +19,15 @@ mongo.getCollection = function (callbackSuccess, callbackError) {
             //    collection = db.collection('sample');
         });
 }
+
+
+/* "mongodb+srv://chaitu257:cmk357@smartgrid-shqik.mongodb.net/test?retryWrites=true&w=majority"
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://cmk357:<password>@smartgrid-shqik.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+}); */
